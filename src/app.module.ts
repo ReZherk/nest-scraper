44ad9e512285970f848e0241defe3600d722f572
ReZherk <patrickcomeresbueno@gmail.com>
2026-03-10 22:28:40 -0500

    add  config file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 
 import { StaticScraperService } from './scraper/services/static-scraper.service';
 import { ScraperController } from './scraper/scraper.controller';
+import { ConfigModule } from '@nestjs/config';
+import appConfig from './config/app.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig],
+    }),
+  ],
   controllers: [ScraperController],
   providers: [StaticScraperService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { setupSwagger } from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  setupSwagger(app);
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+
+  console.log(`🚀 App running on http://localhost:${port}`);
+  console.log(`📖 Swagger docs available at http://localhost:${port}/api`);
 }
-bootstrap();
+bootstrap()
+  .then(() => {
+    console.log('Aplicación iniciada correctamente');
+  })
+  .catch((err) => {
+    console.error('Error al iniciar la aplicación', err);
+  });
